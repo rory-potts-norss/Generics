249957d57e2a6b022132edc3f8632c9807f2bceb
Rory Potts <rory.potts@raytheon.co.uk>
2024-01-24 12:30:48 +0000

    Add new generic class

--- app/v2/Collection.java
+++ app/v2/Collection.java
@@ -1,4 +1,40 @@
 package app.v2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Collection<T, U> {
+
+    private T player;
+    private List<U> resources = new ArrayList<>();
+    private String name;
+    private int max;
+
+    public Collection(String name, int max, T player) {
+        this.player = player;
+        this.name = name;
+        this.max = max;
+    }
+
+    public Collection<T,U> add(U resource) throws Exception {
+        if (resources.size() == max) {
+            throw new Exception(String.format("%s is full!", name));
+        }
+        resources.add(resource);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+    // @TODO reduce this method to 1 line
+    List<String> names = new ArrayList<>();
+    for (U resource : resources) {
+        names.add(resource.toString());
+    }
+    return String.format("%s's %s: %s", this.player.toString(), this.name, String.join(", ", names));
+    }
+
+    
+
+
 }
